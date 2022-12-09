0501b707f5c996d564ecd04a3f98d535260443ee
Onkar Siddhaye <sidonkar@gmail.com>
2022-12-09 08:33:04 +0530

    Update userScript.js

--- userScript.js
+++ userScript.js
@@ -3,7 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Onkar
+// @match        https://kite.zerodha.com/*
 // @match        https://kite.zerodha.com/positions
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zerodha.com
 // @grant        none
